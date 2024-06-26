19953ac2f2a3660e672687a6dd31bf5949d5ab1f
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-26 08:13:02 -0700

    Error

--- main.py
+++ main.py
@@ -2,16 +2,30 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+  
+    minval, median, maxval = int()
+    
+    if num1 < num2 and num1 < num3:
+        minval = num1
+    else:
+        if num2 < num1 and num2 < num3:
+            minval = num2
+        else:
+            minval = num3
+            
+    median = num1 + num2 + num3 % 3
+            
+    if num1 > num2 and num1 > num3:
+        maxval = num1
+    else:
+        if num2 > num1 and num2 > num3:
+            maxval = num2
+        else:
+            maxval = num3
+            
 
     print(minval, median, maxval)
-    ########################################
-    # Do not delete the return statement
-    ########################################
+    
     return minval, median, maxval
 
 
